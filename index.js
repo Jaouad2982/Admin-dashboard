152f567e331ae9105542d3f21dea48cd4e60b994
Jaouad Sarouti <jaouadsarti@gmail.com>
2026-06-17 12:45:21 -0400

    Add a contact form | share btn function...

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const likes = document.querySelector(".likes");
 const view = document.querySelector(".view");
 const star = document.querySelector(".star");
+const form = document.querySelector(".contact-form");
+const closeForm = document.querySelector(".close-form");
+const share = document.querySelector(".share");
+
 let counter = 0;
 window.addEventListener("load", openModal);
 
@@ -13,4 +17,13 @@ function openModal() {
 closeModal = document.querySelector(".close-modal");
 closeModal.addEventListener("click", function () {
   modal.style.display = "none";
+});
+
+share.addEventListener("click", function () {
+  form.style.display = "block";
+});
+
+closeForm.addEventListener("click", function () {
+  form.style.display = "none";
+  console.log("form clicked");
 });
