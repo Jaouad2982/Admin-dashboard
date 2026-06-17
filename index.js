0873b574ce3237989f170b2111dabd3ab8807e05
Jaouad Sarouti <jaouadsarti@gmail.com>
2026-06-17 11:54:47 -0400

    add a modal

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const likes = document.querySelector(".likes");
 const view = document.querySelector(".view");
 const star = document.querySelector(".star");
 let counter = 0;
+window.addEventListener("load", openModal);
+
+const modal = document.querySelector(".modal");
+console.log(modal.textContent);
+function openModal() {
+  modal.style.display = "block";
+}
+
+closemodal = document.querySelector(".closemodal");
+closemodal.addEventListener("click", function () {
+  modal.style.display = "none";
+});
